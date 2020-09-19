01ef20f1be661f3e48b69eaf28a8042bef62edc8
dipu0102 <50037920+dipu0102@users.noreply.github.com>
2020-09-19 17:32:52 +0530

    Updates

---
+++ precache-manifest.7bb7b642833f897311e0b38cfd830a2b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e79b9fad4152a25d6f9bb50be2d59832",
+    "url": "/reactlive/index.html"
+  },
+  {
+    "revision": "62666f2bb239e7e34031",
+    "url": "/reactlive/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "1e26b0ef8396444469cf",
+    "url": "/reactlive/static/css/main.4e042d2c.chunk.css"
+  },
+  {
+    "revision": "62666f2bb239e7e34031",
+    "url": "/reactlive/static/js/2.46931f24.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactlive/static/js/2.46931f24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e26b0ef8396444469cf",
+    "url": "/reactlive/static/js/main.f30ebb72.chunk.js"
+  },
+  {
+    "revision": "eb2bdb4e11587827658f",
+    "url": "/reactlive/static/js/runtime-main.53f8b0ba.js"
+  },
+  {
+    "revision": "5a1f8c730784f683ff25bf9e7096f3e1",
+    "url": "/reactlive/static/media/img1.5a1f8c73.jpg"
+  },
+  {
+    "revision": "5a1f8c730784f683ff25bf9e7096f3e1",
+    "url": "/reactlive/static/media/img4.5a1f8c73.jpg"
+  },
+  {
+    "revision": "b48e983536b510a1a2e2732727149a32",
+    "url": "/reactlive/static/media/img6.b48e9835.jpg"
+  },
+  {
+    "revision": "713418d0aad9b7e116a158feb704fb65",
+    "url": "/reactlive/static/media/img7.713418d0.jpg"
+  },
+  {
+    "revision": "0df3e875f0fc95d4346fbd5f548bce5f",
+    "url": "/reactlive/static/media/img8.0df3e875.jpg"
+  },
+  {
+    "revision": "96ddff371b73f85467536ea0a864f2e5",
+    "url": "/reactlive/static/media/img9.96ddff37.jpg"
+  },
+  {
+    "revision": "bca0c698e12d306437197ba512e9367e",
+    "url": "/reactlive/static/media/unnamed.bca0c698.jpg"
+  }
+]);
